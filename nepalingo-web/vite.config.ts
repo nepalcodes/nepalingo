6b3a65a745f6f7eec50c18458b5437c8c80a4899
NancyAanchal <nancyjha2006@gmail.com>
2024-07-10 21:01:32 +0545

    fixed another minor error

--- nepalingo-web/vite.config.ts
+++ nepalingo-web/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "path";
 import tailwindcss from "tailwindcss";
-import stringPlugin from "vite-plugin-string";
 
 export default defineConfig({
   plugins: [
