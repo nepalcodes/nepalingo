cc4b769757c4947081b9124fb248430ba215415b
binamkayastha <binamkayastha@gmail.com>
2024-07-13 18:56:12 -0400

    Add FlashcardPage route

--- nepalingo-web/src/App.tsx
+++ nepalingo-web/src/App.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import User_auth from "./components/userAuth/UserAuth";
 import Home from "./pages/Home/Home";
+import FlashcardPage from "./pages/Flashcards.tsx"
 import supabase from "./components/userAuth/supabaseClient";
 import ReactGA from 'react-ga4';
 
@@ -42,6 +43,7 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/login" element={<User_auth />} />
+        <Route path="/learn" element={<FlashcardPage />} />
         {/* Protect the / route, redirect to /login if not authenticated */}
         <Route
           path="/"
