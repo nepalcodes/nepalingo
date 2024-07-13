83e984bcf0ad33c8ac98ff4fa97d30ed1aa1217a
binamkayastha <binamkayastha@gmail.com>
2024-07-13 18:55:30 -0400

    Update Header component with logo

--- nepalingo-web/src/components/Header.tsx
+++ nepalingo-web/src/components/Header.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import styles from './header.module.css';
+import React from "react";
+import styles from "./header.module.css";
+import logo from "../assets/NepaLingoLogoWhiteBg.jpg";
+
 const Header: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <h1 className="text-3xl font-bold underline">Hello, World!</h1>
-      </header>
-    </div>
+    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+        <a href="https://nepalingo.com" className="flex items-center">
+          <img src={logo} alt="Nepalingo Logo" className="h-24" />
+        </a>
+        <div className="flex items-center lg:order-2">
+          <a
+            href="#"
+            className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+          >
+            {/* TODO: Sign out logic */}
+            Sign Out
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 };
 
