504c7dbb44197c68505a5b7f3d8d699d68a0ba17
binamkayastha <binamkayastha@gmail.com>
2024-07-13 19:09:17 -0400

    Update header to use tailwind instead of module.css

--- nepalingo-web/src/components/Header.tsx
+++ nepalingo-web/src/components/Header.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import styles from "./header.module.css";
 import logo from "../assets/NepaLingoLogoWhiteBg.jpg";
 
 const Header: React.FC = () => {
   return (
-    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 text-center">
       <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
         <a href="https://nepalingo.com" className="flex items-center">
           <img src={logo} alt="Nepalingo Logo" className="h-24" />
