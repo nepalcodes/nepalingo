dd288883657dabc989e203f5791e5d8597b1afc8
saphal-upreti <108993335+saphal-upreti@users.noreply.github.com>
2024-07-16 20:25:42 +0545

    detect clicks on flashcards and page view on flashcard page

--- nepalingo-web/src/pages/FlashcardPage.tsx
+++ nepalingo-web/src/pages/FlashcardPage.tsx
@@ -1,8 +1,14 @@
 import Flashcard from "../components/Flashcard";
 import Header from "../components/header/Header";
 import React from "react";
+import ReactGA from "react-ga4";
 
 const FlashcardPage: React.FC = () => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+    title: "flashcard page",
+  });
   return (
     <>
       <Header />
