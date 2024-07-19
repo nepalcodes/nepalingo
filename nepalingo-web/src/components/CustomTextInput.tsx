87f882558a4070fbadcb68199b5fa64a3c62799d
Sanskar <lamsalsskr@gmail.com>
2024-07-19 21:51:34 +0545

    Custom Text Input

--- nepalingo-web/src/components/CustomTextInput.tsx
+++ nepalingo-web/src/components/CustomTextInput.tsx
@@ -7,7 +7,6 @@ import {
 interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: string;
     value?: string;
-    onChangeText?: (text: string) => void;
     iconProps: FontAwesomeIconProps;
     placeholder?: string;
     error?: string;
@@ -17,14 +16,14 @@ interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 export const CustomTextInput = ({
     label,
-    onChangeText,
     iconProps,
     placeholder,
     name,
     inputType,
     className,
+    error,
+    ...props
 }: TextInputProps) => {
-    const inputRef = useRef<null | HTMLInputElement>(null);
     return (
         <div className={className}>
             {label && (
@@ -38,13 +37,12 @@ export const CustomTextInput = ({
             <div className="relative mb-6 w-full">
                 <FontAwesomeIcon {...iconProps} />
                 <input
-                    ref={inputRef}
                     type={inputType}
                     name={name}
                     className={`p-4 ${iconProps ? "ps-12" : ""
                         } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block w-full   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-primary `}
                     placeholder={placeholder}
-                    onChange={(e) => onChangeText && onChangeText(e.target.value)}
+                    {...props}
                 />
             </div>
         </div>
