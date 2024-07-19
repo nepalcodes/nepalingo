0834fb2f788bb633625d3d159c0897b3fe0b51b2
binamkayastha <binamkayastha@gmail.com>
2024-07-18 21:23:53 -0400

    Fixup error message grammar

--- nepalingo-web/src/hooks/useDictionary.tsx
+++ nepalingo-web/src/hooks/useDictionary.tsx
@@ -33,13 +33,13 @@ const useDictionary = ({ language, ...otherProps }: DictionaryProps) => {
             return useNewari(otherProps)
         // case 'tajpuriya':
         //     return ({
-        //         error: { message: "Sorry the language doesnot exists" },
+        //         error: { message: "Sorry the language does not exist" },
         //         data: null,
         //         isLoading: false,
         //     })
         default:
             return ({
-                error: { message: "Sorry the language doesnot exists" },
+                error: { message: "Sorry the language does not exist" },
                 data: undefined,
                 isLoading: false,
             })
