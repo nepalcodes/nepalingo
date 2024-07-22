ea125c37284452da084c218b1c609405d176656f
binamkayastha <binamkayastha@gmail.com>
2024-07-22 01:34:44 -0400

    Fixes issues with daily streak bug
    Without this fix, refreshing the page would cause the streak to be reset to 1, due to the update function not having fetched the data before updating it.

--- nepalingo-web/src/hooks/StreakContext.tsx
+++ nepalingo-web/src/hooks/StreakContext.tsx
@@ -24,8 +24,6 @@ export const StreakProvider = ({ children }: { children: ReactNode }) => {
   const { user } = useAuth();
   const [currentStreak, setCurrentStreak] = useState(0);
   const [longestStreak, setLongestStreak] = useState(0);
-  const [streakStartDate, setStreakStartDate] = useState(0);
-  const [streakEndDate, setStreakEndDate] = useState<string | null>(null);
 
   const fetchStreakData = async () => {
     if (user) {
@@ -38,44 +36,49 @@ export const StreakProvider = ({ children }: { children: ReactNode }) => {
       if (error && error.code !== "PGRST116") {
         console.error("Error fetching streak data:", error);
       }
+      return data;
+    }
+  };
 
-      if (data) {
-        setStreakStartDate(data.streak_start_date);
-        setCurrentStreak(data.current_streak);
-        setLongestStreak(data.longest_streak);
-        setStreakEndDate(data.streak_end_date);
-      }
+  const updateStreakState = async () => {
+    const data = await fetchStreakData();
+
+    if (data) {
+      setCurrentStreak(data.current_streak);
+      setLongestStreak(data.longest_streak);
     }
   };
 
   useEffect(() => {
-    fetchStreakData();
+    updateStreakState();
   }, [user]);
 
   const updateStreak = async () => {
+    const data = await fetchStreakData();
     const currentDate = new Date().toISOString().split("T")[0]; // Get current date
 
     if (user) {
       let newStreak = 1;
       let newLongestStreak = 1;
 
-      if (streakEndDate) {
-        const lastDate = new Date(streakEndDate).toISOString().split("T")[0];
+      if (data?.streak_end_date) {
+        const lastDate = new Date(data.streak_end_date)
+          .toISOString()
+          .split("T")[0];
         if (lastDate === currentDate) {
           return; // No update needed if the user has already visited today
         } else if (
           new Date(currentDate).getTime() - new Date(lastDate).getTime() ===
           86400000
         ) {
-          newStreak = currentStreak + 1;
-          newLongestStreak = Math.max(newStreak, longestStreak);
+          newStreak = data.current_streak + 1;
+          newLongestStreak = Math.max(newStreak, data.longest_streak);
+          setCurrentStreak(newStreak);
+          setLongestStreak(newLongestStreak);
         }
       }
 
       // Update state
-      setCurrentStreak(newStreak);
-      setLongestStreak(newLongestStreak);
-      setStreakEndDate(currentDate);
 
       // Upsert database
       const { error } = await supabaseClient
@@ -83,7 +86,8 @@ export const StreakProvider = ({ children }: { children: ReactNode }) => {
         .upsert(
           {
             user_id: user.id,
-            streak_start_date: newStreak === 1 ? currentDate : streakStartDate,
+            streak_start_date:
+              newStreak === 1 ? currentDate : data?.streak_start_date,
             streak_end_date: currentDate, // Update streak_end_date with currentDate
             current_streak: newStreak,
             longest_streak: newLongestStreak,
