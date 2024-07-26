0353b723f8223274d8d1af2d1c9a48e773335788
Aditya Bikram Thakur <adthakur08@gmail.com>
2024-07-26 14:42:34 +0545

    completed basic structure of the card, ammendements still left

--- nepalingo-web/src/components/Card.tsx
+++ nepalingo-web/src/components/Card.tsx
@@ -43,21 +43,19 @@ const Card: React.FC<CardProps> = ({
 
   return (
     <div
-      className={`relative rounded-[43px] overflow-hidden shadow-2xl ${
-        viewType === 1 ? "w-[519px] h-[600px]" : "w-[519px] h-[510px]"
-      }`}
+      className={`relative rounded-[20px] overflow-hidden shadow-2xl w-[519px] h-[600px]`}
       style={{ perspective: "1000px" }}
     >
       {/* Front View */}
       <div
-        className={`absolute inset-0 bg-[#c8383c] text-[#ffffff] flex justify-center items-center ${
+        className={`absolute inset-0 bg-[#4f42d8] text-[#ffffff] flex justify-center items-center ${
           viewType === 0 ? "block" : "hidden"
         }`}
         style={{
           transition: "transform 0.6s",
         }}
       >
-        {viewType === 0 && <div className="text-6xl font-bold">{Word}</div>} {/* Increased text size */}
+        {viewType === 0 && <div className="text-6xl font-bold">{Word}</div>}
       </div>
 
       {/* Second View */}
@@ -70,9 +68,8 @@ const Card: React.FC<CardProps> = ({
         }}
       >
         <div
-          className="relative bg-[#DC143C] text-[#ffffff] w-full rounded-[43px]"
+          className="relative bg-[#ffffff] text-[#000000] w-full h-full rounded-[20px]"
           style={{
-            height: "100%", // Main red div height
             overflow: "hidden",
             display: "flex",
             flexDirection: "column",
@@ -80,29 +77,30 @@ const Card: React.FC<CardProps> = ({
           }}
         >
           <div
-            className="relative bg-[#1a1a1a] w-full rounded-[20px] mx-4 mt-4"
+            className="relative bg-[#4f42d8] w-full h-[60%] overflow-hidden rounded-[20px]"
             style={{
-              overflow: "hidden",
-              flex: "1 1 auto",
               display: "flex",
               flexDirection: "column",
+              justifyContent: "space-between",
+              zIndex: 1, 
             }}
           >
             {ImageUrl && (
-              <div className="relative h-[70%] w-full">
+              <div className="relative w-full h-full rounded-[20px] overflow-hidden">
                 <img
                   src={ImageUrl}
                   alt={Word}
-                  className="absolute top-0 left-0 w-full h-full object-cover rounded-t-[20px]"
+                  className="absolute top-0 left-0 w-full h-full object-cover rounded-[20px]"
+                  style={{ zIndex: -1 }} // Ensuring image appears behind the text
                 />
               </div>
             )}
             <div
-              className="flex flex-col items-center justify-center h-[30%] z-10"
+              className="flex flex-col items-center justify-center h-[60%] z-10"
               style={{ flex: "0 1 auto" }}
             >
-              <p className="text-5xl font-bold">{Word}</p>
-              <p className="text-2xl">{Pronunciation}</p>
+              <p className="text-5xl font-bold text-white">{Word}</p>
+              <p className="text-2xl text-white">{Pronunciation}</p>
               {PronounciationUrl && (
                 <button
                   onClick={handlePronunciation}
@@ -114,8 +112,8 @@ const Card: React.FC<CardProps> = ({
             </div>
           </div>
           <div
-            className="w-full p-4"
-            style={{ borderRadius: '20px 20px 43px 43px' }} // Adjust the radius for the red div
+            className="w-full p-4 rounded-b-[20px]"
+            style={{ borderRadius: "0 0 20px 20px" }}
           >
             <p className="text-2xl font-bold mb-2">Nepali: {TranslatedWord}</p>
             <p className="text-2xl font-bold mb-2">Newari: {DevenagiriSpelling}</p>
@@ -134,7 +132,7 @@ const Card: React.FC<CardProps> = ({
         }}
       >
         <div
-          className="relative bg-[#c8383c] text-[#ffffff] w-full h-full rounded-[43px]"
+          className="relative bg-[#4f42d8] text-[#ffffff] w-full h-full rounded-[20px]"
           style={{
             overflow: "hidden",
             display: "flex",
@@ -142,14 +140,15 @@ const Card: React.FC<CardProps> = ({
           }}
         >
           {ImageUrl && (
-            <div className="relative h-[70%] w-full">
+            <div className="relative h-[70%] w-full rounded-[20px] overflow-hidden">
               <img
                 src={ImageUrl}
                 alt={Word}
-                className="absolute top-0 left-0 w-full h-full object-cover rounded-[43px]"
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-[20px]"
+                style={{ zIndex: 1 }} // Ensuring image appears behind the text
               />
             </div>
-          )}
+          )}  
           <div
             className="flex flex-col items-center justify-center h-[30%] z-10"
             style={{ flex: "0 1 auto" }}
--- nepalingo-web/src/components/Flashcard.tsx
+++ nepalingo-web/src/components/Flashcard.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp, faEye, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
 import Card from "@/components/Card";
-import Button from "@/components/Button";
 import useDictionary from "@/hooks/useDictionary";
 import { generate } from "random-words";
 import ReactGA from "react-ga4";
@@ -9,9 +10,9 @@ const Flashcard: React.FC = () => {
   ReactGA.event({
     category: "flash cards",
     action: "Click",
-    value: 99, // optional, must be a number
-    nonInteraction: true, // optional, true/false
-    transport: "xhr", // optional, beacon/xhr/image
+    value: 99,
+    nonInteraction: true,
+    transport: "xhr",
   });
 
   const [word, setWord] = useState("salt");
@@ -34,12 +35,12 @@ const Flashcard: React.FC = () => {
     setViewType(0);
   };
 
-  if (isLoading) return <div>Loading</div>;
+  if (isLoading) return <div>Loading...</div>;
   if (error?.response?.length) handleNextWord();
   const meaning = data && data.meanings[0];
 
   return (
-    <div className="max-w-md mx-auto p-4 flex justify-center">
+    <div className="max-w-md mx-auto p-4 flex flex-col items-center">
       <div className="mx-auto max-w-[calc(100% - 20px)]">
         {error ? (
           <div>Error: {error.message}</div>
@@ -55,21 +56,31 @@ const Flashcard: React.FC = () => {
           />
         )}
 
-        <div className="flex justify-between mt-4">
-          <Button
+        <div className="flex justify-center mt-4 space-x-2">
+          <button
             disabled={isLoading}
-            className="bg-purple-800"
-            onClick={handleFlip}
-          >
-            Flip
-          </Button>
-          <Button
-            disabled={isLoading}
-            className="bg-gray-500"
+            className="bg-white text-red-500 p-4 rounded-[16px] shadow-md hover:bg-red-500 hover:text-white flex items-center justify-center"
             onClick={handleNextWord}
+            style={{ width: "50px", height: "50px" }}
           >
-            Next Word
-          </Button>
+            <FontAwesomeIcon icon={faThumbsDown} size="lg" />
+          </button>
+          <button
+            disabled={isLoading}
+            className="bg-white text-gray-500 p-4 rounded-[16px] shadow-md hover:bg-gray-500 hover:text-white flex items-center justify-center"
+            onClick={handleFlip}
+            style={{ width: "50px", height: "50px" }}
+          >
+            <FontAwesomeIcon icon={faEye} size="lg" />
+          </button>
+          <button
+            disabled={isLoading}
+            className="bg-white text-green-500 p-4 rounded-[16px] shadow-md hover:bg-green-500 hover:text-white flex items-center justify-center"
+            onClick={handleNextWord}
+            style={{ width: "50px", height: "50px" }}
+          >
+            <FontAwesomeIcon icon={faThumbsUp} size="lg" />
+          </button>
         </div>
       </div>
     </div>
