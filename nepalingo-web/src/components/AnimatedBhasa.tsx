f64aecc609e09128bd3acbc572b9455340a181c1
Aditya Bikram Thakur <adthakur08@gmail.com>
2024-08-03 17:16:56 +0545

    Prettier fixed

--- nepalingo-web/src/components/AnimatedBhasa.tsx
+++ nepalingo-web/src/components/AnimatedBhasa.tsx
@@ -1,5 +1,5 @@
-import * as React from "react"
-import { SVGProps } from "react"
+import * as React from "react";
+import { SVGProps } from "react";
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -353,6 +353,5 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     </g>
     <script />
   </svg>
-)
-export default SvgComponent
-    
+);
+export default SvgComponent;
