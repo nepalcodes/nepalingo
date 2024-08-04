463340bb956e7ca8c87d71d10990cfae95fcc347
Sanskar <lamsalsskr@gmail.com>
2024-08-04 12:33:07 +0545

    Changed home Layout managed padings and added responsiveness for large screen

--- nepalingo-web/src/components/header/Header.tsx
+++ nepalingo-web/src/components/header/Header.tsx
@@ -5,7 +5,7 @@ import ChangeLanguage from "@/components/header/ChangeLanguage";
 
 const Header: React.FC = () => {
   return (
-    <nav className=" border-gray-200 px-6 py-4 lg:px-6 py-1 bg-black text-white flex justify-between items-center w-full h-16">
+    <nav className=" border-gray-200 py-4 py-1 bg-black text-white flex justify-between items-center w-full h-16">
       <div className="flex-1">
         <a href="https://nepalingo.com">
           <img src={logo} alt="Nepalingo Logo" className="h-12" />
