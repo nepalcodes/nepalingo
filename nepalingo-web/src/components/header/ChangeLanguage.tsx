aa9e593a6ebf6e419a9345996d1a7b0a32d6a7ba
Sanskar <lamsalsskr@gmail.com>
2024-08-04 14:19:38 +0545

    fixed quotes type and added correct type for selectedLanguage

--- nepalingo-web/src/components/header/ChangeLanguage.tsx
+++ nepalingo-web/src/components/header/ChangeLanguage.tsx
@@ -2,65 +2,65 @@ import React, { useEffect, useRef, useState } from "react";
 import Menu from "./Menu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import { LanguageKey, Languages, useLanguage } from "../../hooks/Langauge";
+import { LanguageKey, Languages, useLanguage } from "@/hooks/Langauge";
 
 const ChangeLanguage: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const { selectedLanguage, switchLanguage } = useLanguage();
-  const dropdownRef = useRef<HTMLDivElement>(null);
+    const [isOpen, setIsOpen] = useState(false);
+    const { selectedLanguage, switchLanguage } = useLanguage();
+    const dropdownRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const handleEscape = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
+    useEffect(() => {
+        const handleEscape = (event: KeyboardEvent) => {
+            if (event.key === "Escape") {
+                setIsOpen(false);
+            }
+        };
+        const handleOutsideClick = (event: MouseEvent) => {
+            if (!dropdownRef.current?.contains(event.target as Node)) {
+                setIsOpen(false);
+            }
+        };
+        if (isOpen) {
+            document.addEventListener("click", handleOutsideClick);
+        } else {
+            document.removeEventListener("click", handleOutsideClick);
+        }
+
+        window.addEventListener("keydown", handleEscape);
+
+        return () => {
+            window.removeEventListener("keydown", handleEscape);
+            document.removeEventListener("click", handleOutsideClick);
+        };
+    }, [isOpen]);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleSelect = (option: LanguageKey) => {
+        switchLanguage(option);
         setIsOpen(false);
-      }
     };
-    const handleOutsideClick = (event: MouseEvent) => {
-      if (!dropdownRef.current?.contains(event.target as Node)) {
-        setIsOpen(false);
-      }
-    };
-    if (isOpen) {
-      document.addEventListener("click", handleOutsideClick);
-    } else {
-      document.removeEventListener("click", handleOutsideClick);
-    }
-
-    window.addEventListener("keydown", handleEscape);
-
-    return () => {
-      window.removeEventListener("keydown", handleEscape);
-      document.removeEventListener("click", handleOutsideClick);
-    };
-  }, [isOpen]);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleSelect = (option: LanguageKey) => {
-    switchLanguage(option);
-    setIsOpen(false);
-  };
-  const options = Object.keys(Languages).map((key) => ({
-    label: key,
-    value: Languages[key as LanguageKey],
-  }));
-  return (
-    <div ref={dropdownRef} className="relative text-left h-full">
-      <button
-        type="button"
-        className="w-48 bg-grayDark px-4 h-10 rounded-lg text-sm font-medium text-gray-900 focus:outline-none flex justify-between items-center"
-        onClick={toggleMenu}
-      >
-        <span className="text-white font-primary font-black">
-          {selectedLanguage}
-        </span>
-        <FontAwesomeIcon icon={faChevronDown} className="text-white ml-2" />
-      </button>
-      <Menu isOpen={isOpen} onSelect={handleSelect} options={options} />
-    </div>
-  );
+    const options = Object.keys(Languages).map((key) => ({
+        label: key,
+        value: Languages[key as LanguageKey],
+    }));
+    return (
+        <div ref={dropdownRef} className="relative text-left h-full">
+            <button
+                type="button"
+                className="w-48 bg-grayDark px-4 h-10 rounded-lg text-sm font-medium text-gray-900 focus:outline-none flex justify-between items-center"
+                onClick={toggleMenu}
+            >
+                <span className="text-white font-primary font-black">
+                    {selectedLanguage}
+                </span>
+                <FontAwesomeIcon icon={faChevronDown} className="text-white ml-2" />
+            </button>
+            <Menu isOpen={isOpen} onSelect={handleSelect} options={options} />
+        </div>
+    );
 };
 
 export default ChangeLanguage;
