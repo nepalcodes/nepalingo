9a8cf05eff02b137ca5f7b5cd71d0c587e0082ee
Sanskar <lamsalsskr@gmail.com>
2024-08-04 12:29:34 +0545

    Made activity card resposive

--- nepalingo-web/src/components/ActivityCard.tsx
+++ nepalingo-web/src/components/ActivityCard.tsx
@@ -20,15 +20,15 @@ const ActivityCard: React.FC<ActivityCardProps> = ({
   onClick,
 }) => {
   return (
-    <div className="relative daily-quiz-card text-white p-4 sm:p-6 md:p-8 rounded-lg flex flex-col justify-end h-[180px] sm:h-[200px] md:h-[220px] pb-5 overflow-hidden">
+    <div className="relative daily-quiz-card text-white p-4 sm:p-4 md:p-4 rounded-lg flex flex-col justify-end h-full min-h-32 h-48 pb-5 overflow-hidden">
       <img
         src={backgroundImageUrl}
         alt="Background"
         className="absolute inset-0 w-full h-full object-cover"
       />
       <div className="absolute inset-0 bg-black/80" />
-      <div className="relative flex flex-col justify-between h-full">
-        <div className="flex-1 flex flex-col justify-end">
+      <div className="relative flex flex-row flex-wrap justify-between items-end ">
+        <div className="flex min-w-24  flex-col ">
           <p className="text-base sm:text-lg font-bold mb-1">
             {quizYourselfText}
           </p>
@@ -39,7 +39,7 @@ const ActivityCard: React.FC<ActivityCardProps> = ({
         {children}
         {buttonText && onClick && (
           <Button
-            className="bg-red-600 text-white font-bold py-1 px-3 sm:py-2 sm:px-5 md:py-3 md:px-6 rounded text-xs sm:text-sm md:text-base"
+            className="bg-red-600 block w-fit h-fit text-white font-bold py-1 px-3 sm:py-2 sm:px-5 md:py-3 md:px-6 rounded text-xs sm:text-sm md:text-base"
             smallHeight={true}
             onClick={onClick}
           >
