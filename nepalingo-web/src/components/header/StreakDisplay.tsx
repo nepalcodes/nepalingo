67184e02872f093635f2aa71d9e25993bf7b5ed1
Sanskar <lamsalsskr@gmail.com>
2024-08-04 12:29:17 +0545

    Added \n s instead of manually splitting

--- nepalingo-web/src/components/header/StreakDisplay.tsx
+++ nepalingo-web/src/components/header/StreakDisplay.tsx
@@ -4,27 +4,24 @@ import { getPhrase } from "@/components/header/StreakPhrase";
 import fire from "@/assets/fire.svg";
 
 const StreakDisplay: React.FC = () => {
-  const { currentStreak } = useContext(StreakContext);
-  const phrase = getPhrase(currentStreak);
+    const { currentStreak } = useContext(StreakContext);
+    const phrase = getPhrase(currentStreak);
 
-  return (
-    <div className="bg-[#D03641] min-w-[210px] max-w-max h-[55px] rounded-md px-3 pt-0 inline-flex items-center justify-between text-white">
-      <div className="text-left flex-1">
-        <p className="text-sm font-bold leading-tight uppercase">
-          {phrase.split("!")[0]}!
-        </p>
-        <p className="text-sm font-bold leading-tight uppercase">
-          {phrase.split("!")[1]}!
-        </p>
-      </div>
-      <div className="relative flex-shrink-0">
-        <img src={fire} alt="Fire" className="w-10 h-10" />
-        <span className="absolute font-black font-primary text-1xl text-white bottom-0 left-1/2 transform -translate-x-1/2">
-          {currentStreak}
-        </span>
-      </div>
-    </div>
-  );
+    return (
+        <div className="bg-primary min-w-32  h-fit rounded-md px-3 py-2 inline-flex items-center justify-between text-white">
+            <div className="text-left flex-1">
+                <p className="text-sm font-bold uppercase whitespace-pre">
+                    {phrase}
+                </p>
+            </div>
+            <div className="relative flex-shrink-0">
+                <img src={fire} alt="Fire" className="w-10 h-10" />
+                <span className="absolute font-black font-primary text-1xl text-white bottom-0 left-1/2 transform -translate-x-1/2">
+                    {currentStreak}
+                </span>
+            </div>
+        </div>
+    );
 };
 
 export default StreakDisplay;
