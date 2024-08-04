fa5d6ece344c5e97b4bce4325c555f6dfb9a5cdf
Sanskar <lamsalsskr@gmail.com>
2024-08-04 11:02:57 +0545

    Fixed quotes not loading issue. Added a function to correctly convert rows into quote and translation (Basically the fix was removing the quoteText and calling the function directly)

--- nepalingo-web/src/hooks/useQuotes.tsx
+++ nepalingo-web/src/hooks/useQuotes.tsx
@@ -1,63 +1,55 @@
 import { useState, useEffect } from "react";
+import { useLanguage } from "./Langauge";
 
 interface Quote {
-  text: string;
-  translation: string;
+    text: string;
+    translation: string;
 }
 export interface QuotesResponse {
-  quotes: Quote[];
-  randomQuote: Quote | null;
+    quotes: Quote[];
+    randomQuote: Quote | null;
 }
 
 interface useQuotesProps {
-  language: string;
+    language: string;
 }
 
-const useQuotes = ({ language }: useQuotesProps): QuotesResponse => {
-  const [quotes, setQuotes] = useState<Quote[]>([]);
-  const [quotesText, setQuotesText] = useState("");
-  const [randomQuote, setRandomQuote] = useState<Quote | null>(null);
+const useQuotes = (): QuotesResponse => {
+    const [quotes, setQuotes] = useState<Quote[]>([]);
+    const [randomQuote, setRandomQuote] = useState<Quote | null>(null);
+    const { selectedLanguage } = useLanguage()
 
-  useEffect(() => {
-    let sourceFile = "";
 
-    switch (language) {
-      case "Newari":
-        sourceFile = "/quotes/newari.csv";
-        break;
-      case "Tajpuria":
-        sourceFile = "/quotes/tajpuriya.csv";
-        break;
-      case "Maithili":
-        sourceFile = "/quotes/maithili.csv";
-        break;
-      default:
-        sourceFile = "";
-    }
 
-    if (sourceFile) {
-      fetch(sourceFile)
-        .then((r) => r.text())
-        .then((text) => {
-          setQuotesText(text);
-        })
-        .catch((error) => {
-          console.error("Error fetching quotes:", error);
+    function parse(row: string) {
+        var insideQuote = false,
+            entries = [],
+            entry: Array<string> = [];
+        row.split('').forEach(function(character) {
+            if (character === '"') {
+                insideQuote = !insideQuote;
+            } else {
+                if (character == "," && !insideQuote) {
+                    entries.push(entry.join(''));
+                    entry = [];
+                } else {
+                    entry.push(character);
+                }
+            }
         });
+        entries.push(entry.join(''));
+        return entries;
     }
-  }, [language]);
-
-  useEffect(() => {
-    if (quotesText) {
-      const loadQuotes = () => {
-        const quotesArray = quotesText.split("\n").map((line: string) => {
-          const [quote, englishTranslation] = line.split(",");
-          return {
-            text: quote ? quote.trim().replace(/(^"|"$)/g, "") : "",
-            translation: englishTranslation
-              ? englishTranslation.trim().replace(/(^"|"$)/g, "")
-              : "",
-          };
+    const loadQuotes = (quotesText: string) => {
+        const quotesArray = quotesText.split(/\r\n|\n/).map((line: string) => {
+            console.log(line)
+            const [quote, englishTranslation] = parse(line)
+            return {
+                text: quote ? quote.trim().replace(/(^"|"$)/g, "") : "",
+                translation: englishTranslation
+                    ? englishTranslation.trim().replace(/(^"|"$)/g, "")
+                    : "",
+            };
         });
 
         const randomIndex = Math.floor(Math.random() * quotesArray.length);
@@ -65,13 +57,38 @@ const useQuotes = ({ language }: useQuotesProps): QuotesResponse => {
 
         setQuotes(quotesArray);
         setRandomQuote(randomQuote);
-      };
+    };
 
-      loadQuotes();
-    }
-  }, [quotesText]);
+    useEffect(() => {
+        let sourceFile = "";
+        switch (selectedLanguage) {
+            case "Newari":
+                sourceFile = "/quotes/newari.csv";
+                break;
+            case "Tajpuria":
+                sourceFile = "/quotes/tajpuriya.csv";
+                break;
+            case "Maithili":
+                sourceFile = "/quotes/maithili.csv";
+                break;
+            default:
+                sourceFile = "";
+        }
 
-  return { quotes, randomQuote };
+        if (sourceFile) {
+            fetch(sourceFile)
+                .then((r) => r.text())
+                .then((text) => {
+                    loadQuotes(text);
+                })
+                .catch((error) => {
+                    console.error("Error fetching quotes:", error);
+                });
+        }
+    }, [selectedLanguage]);
+
+
+    return { quotes, randomQuote };
 };
 
 export default useQuotes;
