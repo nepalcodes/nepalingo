7559903dd3994d545506f6d876dabc14d8d36218
NancyAanchal <nancyjha2006@gmail.com>
2024-08-09 20:36:26 +0545

    tried fixign forgot-passowrd-2

--- nepalingo-web/src/hooks/Auth.tsx
+++ nepalingo-web/src/hooks/Auth.tsx
@@ -28,8 +28,8 @@ const AuthContext = createContext<AuthContextProps>({
   signIn: (data) => supabaseClient.auth.signInWithPassword(data),
   resetPasswordEmail: (email) =>
     supabaseClient.auth.resetPasswordForEmail(email, {
-      //redirectTo: "https://www.nepalingo.com/reset-password",
-      redirectTo: "http://localhost:5173/reset-password",
+      redirectTo: "https://www.nepalingo.com/reset-password",
+      //redirectTo: "http://localhost:5173/reset-password",
     }),
   resetPassword: (password) => supabaseClient.auth.updateUser({ password }),
 });
@@ -74,8 +74,8 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     signIn: (data) => supabaseClient.auth.signInWithPassword(data),
     resetPasswordEmail: (email) =>
       supabaseClient.auth.resetPasswordForEmail(email, {
-        //redirectTo: "https://www.nepalingo.com/reset-password",
-        redirectTo: "http://localhost:5173/reset-password",
+        redirectTo: "https://www.nepalingo.com/reset-password",
+        //redirectTo: "http://localhost:5173/reset-password",
       }),
     resetPassword: async (password: string) => {
       // Attempt to restore the session using the access token in the URL
