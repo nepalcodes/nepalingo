1650d399f5a4ed3a12b6a693dcdde9070c9fff0e
binamkayastha <binamkayastha@gmail.com>
2024-08-11 13:13:11 +0545

    Copy config from example

--- nepalingo-web/vite.config.ts
+++ nepalingo-web/vite.config.ts
@@ -4,7 +4,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "path";
 import tailwindcss from "tailwindcss";
 
-export default defineConfig({
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
   plugins: [react(), tsconfigPaths()],
   css: {
     postcss: {
@@ -16,4 +19,26 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
     },
   },
-});
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
